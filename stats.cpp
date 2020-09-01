52a14831b0ae34cea79f36f04ad718822c006e09
Tanmay <68606780+TnmyB@users.noreply.github.com>
2020-09-01 07:29:25 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,36 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& numbers) {
     //Implement statistics here
+    Stats answers;
+
+    if (numbers.size() == 0) {
+        answers.average = NAN;
+        answers.min = NAN;
+        answers.max = NAN;
+    }
+
+    float sum = 0;
+    float min = numbers[0];
+    float max = numbers[0];
+
+    for (auto i : numbers) {
+        sum += i;
+
+        if (min > i) {
+            min = i;
+        }
+
+        if (max < i) {
+            max = i;
+        }
+    }
+
+    if (numbers.size() != 0) {
+        answers.min = min;
+        answers.max = max;
+        answers.average = (sum / numbers.size());
+    }
+
+    return answer;
 }
