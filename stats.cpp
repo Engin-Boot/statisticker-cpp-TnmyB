3e91d6f077db857ee7a95017b78e68f9c558951f
Tanmay <68606780+TnmyB@users.noreply.github.com>
2020-09-01 08:08:04 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,46 +1,46 @@
-#include "stats.h"
+    #include "stats.h"
 
 
-float avg(const std::vector<float>& numbers) {
-    float sum = 0;
-    for (auto i : numbers) {
-        sum += (*i);
+    float avg(const std::vector<float>& numbers) {
+        float sum = 0;
+        for (int i=0;i<numbers.size();i++) {
+            sum += numbers[i];
+        }
+        return sum / numbers.size();
     }
-    return sum / numbers.size();
-}
 
-float min(const std::vector<float>& numbers){
-float min = 9999;
-for (auto i : numbers) {
-    if ((*i) < min)
-        min = (*i);
-}
-return min;
-}
+    float min(const std::vector<float>& numbers){
+    float min = 9999;
+    for (int i=0;i<numbers.size();i++) {
+        if (numbers[i] < min)
+            min = numbers[i];
+    }
+    return min;
+    }
 
-float max(const std::vector<float>& numbers){
-float max = -9999;
-for (auto i : numbers) {
-    if ((*i)>max)
-        max = (*i);
-}
-return max;
-}
+    float max(const std::vector<float>& numbers){
+    float max = -9999;
+    for (int i=0;i<numbers.size();i++) {
+        if (numbers[i]>max)
+            max = numbers[i];
+    }
+    return max;
+    }
 
-Stats Statistics::ComputeStatistics(const std::vector<float>& numbers) {
-    //Implement statistics here
-    Stats answers;
+    Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& numbers) {
+        //Implement statistics here
+        Stats answers;
 
-    if (numbers.size() == 0) {
-        answers.average = NAN;
-        answers.min = NAN;
-        answers.max = NAN;
+        if (numbers.size() == 0) {
+            answers.average = NAN;
+            answers.min = NAN;
+            answers.max = NAN;
+            return answers;
+        }
+
+        answers.average = avg(numbers);
+        answers.min = min(numbers);
+        answers.max = max(numbers);
+
         return answers;
     }
-
-    answers.average = avg(numbers);
-    answers.min = min(numbers);
-    answers.max = max(numbers);
-
-    return answers;
-}
